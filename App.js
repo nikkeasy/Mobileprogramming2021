e1145a241149d11a62e13ef30595afaf8be71532
Nikolas Kunnas <bgh302@myy.haaga-helia.fi>
2021-01-25 00:25:28 +0200

    First commit. Added a basic button for android

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+// Luodaan funktio joka palautetaan return-lausekkeessa
+const App = () =>  {
+  const createAlertingButton = () => 
+    Alert.alert(
+      "Attention!", 
+      "You have pressed T H E button!!!!!!",
+      [
+        { // Kaksi eri vaihtoehtoa, Accept ja NOPE
+          text: "Accept",  
+          onPress: () => console.log("Accepted pressed")
+        },
+        { text: "NOPE!", onPress: () => console.log("Nope pressed") }
+      ]
+    ); 
 
-export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Click the button to see something fun!</Text>
+      
+      <Button
+      title="Hello World!"
+      color="#f194ff"
+      onPress={createAlertingButton} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
+
+export default App; 
